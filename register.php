be18039e4882c952b34b1202510377574f8193f3
faiz-dev <a.faizmail@gmail.com>
2026-01-05 15:09:58 +0700

    register page

--- register.php
+++ register.php
@@ -12,20 +12,23 @@
     }
   </style>
 </head>
-<title>Register</title>
+<title>Login</title>
 </head>
 
 <body>
   <div class="max-w-[490px] mx-auto mt-[100px] px-6">
     <!-- // top section -->
     <div class="flex flex-col">
-      <span class="self-center p-2 px-3 rounded mx-auto bg-[#F2EBFD]"><i class="hgi hgi-stroke hgi-task-edit-01"></i></span>
-      <h1 class="text-center fs-4xl text-[32px] font-bold">Welcome Back</h1>
-      <p class="text-center max-w-[300px] mx-auto">Please sign in to continue managing your notes and ideas.</p>
+      <h1 class="fs-4xl text-[32px] font-bold">Create Account</h1>
+      <p class="max-w-[300px]">Start capturing your ideas with a fresh perspective</p>
     </div>
 
     <!-- // form section -->
     <form action="./index.php" method="post" class="flex flex-col gap-4 mt-6">
+        <div class="flex flex-col gap-2">
+          <label for="fullname">Fullname</label>
+          <input type="text" name="fullname" id="fullname" class="p-2 bg-[#f8fafc] rounded border border-[#9DABBE]" placeholder="Enter your fullname">
+        </div>
         <div class="flex flex-col gap-2">
           <label for="email">Email</label>
           <input type="email" name="email" id="email" class="p-2 bg-[#f8fafc] rounded border border-[#9DABBE]" placeholder="Enter your email">
@@ -34,14 +37,11 @@
           <label for="password">Password</label>
           <input type="password" name="password" id="password" class="p-2 bg-[#f8fafc] rounded border border-[#9DABBE]" placeholder="Enter your password">
         </div>
-
-        <a href="" class="self-end text-[#7C3AED]">Forgot password?</a>
-
-        <button type="submit" class="bg-[#7C3AED] text-white font-bold p-2 mt-4 hover:bg-[#6b21a8] transition shadow-lg rounded">Sign In</button>
+        <button type="submit" class="bg-[#7C3AED] text-white font-bold p-2 mt-4 hover:bg-[#6b21a8] transition shadow-lg rounded">Register</button>
       </form>
 
       <!-- // bottom section -->
-      <p class="text-center mt-6">Don't have an account? <a href="./register.php" class="text-[#7C3AED] font-bold">Register</a></p>
+      <p class="text-center mt-6">Already have an account? <a href="./index.php" class="text-[#7C3AED] font-bold">Sign In</a></p>
   </div>
 
 </body>
