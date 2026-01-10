0169051f40c4a6d90dede8b3a87a38c177fdd6d1
faiz-dev <a.faizmail@gmail.com>
2026-01-10 14:44:22 +0700

    update forgot-password ui

--- forgot-password.php
+++ forgot-password.php
@@ -19,29 +19,21 @@
   <div class="max-w-[490px] mx-auto mt-[100px] px-6">
     <!-- // top section -->
     <div class="flex flex-col">
-      <h1 class="fs-4xl text-[32px] font-bold">Create Account</h1>
-      <p class="max-w-[300px]">Start capturing your ideas with a fresh perspective</p>
+      <h1 class="fs-4xl text-[32px] font-bold">Forgot Password</h1>
+      <p class="text-[#9DABBE]">No worries! Enter your email address below and we will send you a code to reset your password</p>
     </div>
 
     <!-- // form section -->
     <form action="./index.php" method="post" class="flex flex-col gap-4 mt-6">
         <div class="flex flex-col gap-2">
-          <label for="fullname">Fullname</label>
-          <input type="text" name="fullname" id="fullname" class="p-2 bg-[#f8fafc] rounded border border-[#9DABBE]" placeholder="Enter your fullname">
-        </div>
-        <div class="flex flex-col gap-2">
           <label for="email">Email</label>
           <input type="email" name="email" id="email" class="p-2 bg-[#f8fafc] rounded border border-[#9DABBE]" placeholder="Enter your email">
         </div>
-        <div class="flex flex-col gap-2">
-          <label for="password">Password</label>
-          <input type="password" name="password" id="password" class="p-2 bg-[#f8fafc] rounded border border-[#9DABBE]" placeholder="Enter your password">
-        </div>
-        <button type="submit" class="bg-[#7C3AED] text-white font-bold p-2 mt-4 hover:bg-[#6b21a8] transition shadow-lg rounded">Register</button>
+        <button type="submit" class="bg-[#7C3AED] text-white font-bold p-2 mt-4 hover:bg-[#6b21a8] transition shadow-lg rounded">Send reset link</button>
       </form>
 
       <!-- // bottom section -->
-      <p class="text-center mt-6">Already have an account? <a href="./index.php" class="text-[#7C3AED] font-bold">Sign In</a></p>
+      <p class="text-center mt-6">Remember your password? <a href="./index.php" class="text-[#7C3AED] font-bold">Sign In</a></p>
   </div>
 
 </body>
